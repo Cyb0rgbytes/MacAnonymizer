5dc320dba73ee75dfb1bc52963354b5d20603dc8
SoftAddict <34769900+SoftAddict@users.noreply.github.com>
2019-11-26 03:03:11 -0500

    Update MacAnonymizer.py

--- MacAnonymizer.py
+++ MacAnonymizer.py
@@ -5,19 +5,18 @@
 # Version......: 1.0
 # Usage........: python3 MacAnonymizer.py -i "interface" and -m "targetmac"
 # Python Version.: 3
-
+import re
 import subprocess
 import optparse
 import colorama
 from colorama import Fore, Style
 
-def get_arguments(news=None):
 
+def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
-
 
     if not options.interface:
         parser.error(Fore.RED + "[-] Oopsie, Please specify an interface, use --help for more info.")
@@ -29,11 +28,7 @@ def get_arguments(news=None):
     # code to handle error
 
 
-
-
-
-def change_mac(interface, new_mac, news=None):
-
+def change_mac(interface, new_mac):
     print(Fore.GREEN + "[+] Initializing...")
     print(Fore.GREEN + "[+] Summoning the mac changer wizard")
     print(Fore.GREEN + "[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -42,5 +37,24 @@ def change_mac(interface, new_mac, news=None):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print(Fore.RED + "[-] Could not read MAC address.")
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print(Fore.GREEN + "[+] MAC address successfully changed to " + current_mac)
+else:
+    print(Fore.RED + "[-] MAC address did not get changed.")
